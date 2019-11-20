7df0bb4cfa203cfc7943570d930201dd5bcf2572
Timothy Dai <ttd004@emai>
2019-11-20 09:43:47 -0600

    Repository Moved.

---
+++ Privacy Project/PrivateGallery.swift
@@ -0,0 +1,176 @@
+//
+//  PrivateGallery.swift
+//  Privacy Project
+//
+//  Created by Timothy Dai on 10/31/19.
+//  Copyright © 2019 Capstone. All rights reserved.
+//
+
+import UIKit
+import Photos
+
+class PrivateGallery: UIViewController,UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UINavigationControllerDelegate{
+    
+    // Two variables for the colelction view and
+    // image array
+    var myCollectionView: UICollectionView!
+    var imageArray=[UIImage]()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.title = "Photos"
+               
+                // This initializes the collection view
+               let layout = UICollectionViewFlowLayout()
+               
+               myCollectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+               myCollectionView.delegate=self
+               myCollectionView.dataSource=self
+               myCollectionView.register(PhotoItemCell.self, forCellWithReuseIdentifier: "Cell")
+               myCollectionView.backgroundColor=UIColor.white
+               self.view.addSubview(myCollectionView)
+               
+        myCollectionView.autoresizingMask = UIView.AutoresizingMask(rawValue: UIView.AutoresizingMask.RawValue(UInt8(UIView.AutoresizingMask.flexibleWidth.rawValue) | UInt8(UIView.AutoresizingMask.flexibleHeight.rawValue)))
+               
+           //    grabPhotos()
+        
+    }
+
+    //MARK: CollectionView
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imageArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell=collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! PhotoItemCell
+        cell.img.image=imageArray[indexPath.item]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath)
+        let vc=ImagePreviewVCViewController()
+        vc.imgArray = self.imageArray
+        vc.passedContentOffset = indexPath
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.frame.width
+        //        if UIDevice.current.orientation.isPortrait {
+        //            return CGSize(width: width/4 - 1, height: width/4 - 1)
+        //        } else {
+        //            return CGSize(width: width/6 - 1, height: width/6 - 1)
+        //        }
+        if DeviceInfo.Orientation.isPortrait {
+            return CGSize(width: width/4 - 1, height: width/4 - 1)
+        } else {
+            return CGSize(width: width/6 - 1, height: width/6 - 1)
+        }
+    }
+    
+    //This is for when the device orientation changes
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        myCollectionView.collectionViewLayout.invalidateLayout()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1.0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1.0
+    }
+    
+    //This function is to grab the photos
+    //from public gallery, but it needs to be edited
+    //to only grab pictures the user wants, right now
+    // it grabs all photos from public gallery
+    func grabPhotos(){
+        imageArray = []
+        
+        DispatchQueue.global(qos: .background).async {
+            print("This is run on the background queue")
+            let imgManager=PHImageManager.default()
+            
+            let requestOptions=PHImageRequestOptions()
+            requestOptions.isSynchronous=true
+            requestOptions.deliveryMode = .highQualityFormat
+            
+            let fetchOptions=PHFetchOptions()
+            fetchOptions.sortDescriptors=[NSSortDescriptor(key:"creationDate", ascending: false)]
+            
+            let fetchResult: PHFetchResult = PHAsset.fetchAssets(with: .image, options: fetchOptions)
+            print(fetchResult)
+            print(fetchResult.count)
+            if fetchResult.count > 0 {
+                for i in 0..<fetchResult.count{
+                    imgManager.requestImage(for: fetchResult.object(at: i) as PHAsset, targetSize: CGSize(width:500, height: 500),contentMode: .aspectFill, options: requestOptions, resultHandler: { (image, error) in
+                        self.imageArray.append(image!)
+                    })
+                }
+            } else {
+                print("You got no photos.")
+            }
+            print("imageArray count: \(self.imageArray.count)")
+            
+            DispatchQueue.main.async {
+                print("This is run on the main queue, after the previous code in outer block")
+                self.myCollectionView.reloadData()
+            }
+        }
+    }
+    
+    @IBAction func onGoToPrivacyProject(_ sender: Any) {
+    }
+    
+}
+
+
+// This class is used for each cell of the image
+class PhotoItemCell: UICollectionViewCell {
+    
+    var img = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        img.contentMode = .scaleAspectFill
+        img.clipsToBounds=true
+        self.addSubview(img)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        img.frame = self.bounds
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+struct DeviceInfo {
+    struct Orientation {
+        // indicate current device is in the LandScape orientation
+        static var isLandscape: Bool {
+            get {
+                return UIDevice.current.orientation.isValidInterfaceOrientation
+                    ? UIDevice.current.orientation.isLandscape
+                    : UIApplication.shared.statusBarOrientation.isLandscape
+            }
+        }
+        // indicate current device is in the Portrait orientation
+        static var isPortrait: Bool {
+            get {
+                return UIDevice.current.orientation.isValidInterfaceOrientation
+                    ? UIDevice.current.orientation.isPortrait
+                    : UIApplication.shared.statusBarOrientation.isPortrait
+            }
+        }
+    }
+}
---
+++ Privacy Project/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Privacy Project
+//
+//  Created by Timothy/Ammar/Chandra on 10/17/19.
+//  Copyright © 2019 PETS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func onGoToPrivacyProject(_ sender: Any) {
+    }
+    
+}
